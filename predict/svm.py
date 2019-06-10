c7370c862a55108c6a8e4533bb888c9a7bd9c160
Ray <ray@cryptoterminal.io>
2019-06-10 11:21:24 -0400

    update svm.py for prediction

--- predict/svm.py
+++ predict/svm.py
@@ -10,6 +10,9 @@ from matplotlib import pyplot as plt
 import pickle
 from joblib import dump,load
 
+import warnings
+warnings.filterwarnings("ignore")
+
 # using SVM
 # for details, visit:
 # https://scikit-learn.org/stable/modules/svm.html#regression
@@ -37,10 +40,17 @@ for train, test in rkf.split(features):
 # Train model
 # ****************************************************************
 # read SVM model
-clf = svm.SVR(kernel='RBF', C=1.0, epsilon=0.2)
+#clf = svm.SVR()
+# Fit regression model
+svr_base    = svm.SVR()
+svr_rbf     = svm.SVR(kernel='rbf', C=100, gamma=0.1, epsilon=.1)
+svr_lin     = svm.SVR(kernel='linear', C=100, gamma='auto')
+svr_poly    = svm.SVR(kernel='poly', C=100, gamma='auto', degree=3, epsilon=.1, coef0=1)
 
+# clf.fit(features, target)
 
-#clf.fit(features, target)
+clf = svr_poly
+# Cross-validation 
 scores = cross_val_score(clf, features, target, cv=5)
 print("cross validation score: ", scores)
 print("Accuracy: %0.2f(+/- %0.2f)"%(scores.mean(), scores.std()*2))
@@ -48,6 +58,8 @@ print("Accuracy: %0.2f(+/- %0.2f)"%(scores.mean(), scores.std()*2))
 # ****************************************************************
 # Make cross-validation predictions
 # ****************************************************************
+
+
 predictions = cross_val_predict(clf, features, target, cv=5)
 plt.scatter(target, predictions, edgecolors=(0,0,0))
 plt.plot([min(target),max(target)], [min(target), max(target)], 'k--', lw=4)
@@ -57,6 +69,7 @@ plt.show()
 
 accuracy = metrics.r2_score(target, predictions)
 print("Cross-predicted Accuracy:", accuracy)
+
 # test using the first row
 #test=[8,0,0,1,0.19,0.33,0.02,0.9,0.12,0.17,0.34,0.47,0.29,0.32,0.2,1.0,0.37,0.72,0.34,0.6,0.29,0.15,0.43,0.39,0.4,0.39,0.32,0.27,0.27,0.36,0.41,0.08,0.19,0.1,0.18,0.48,0.27,0.68,0.23,0.41,0.25,0.52,0.68,0.4,0.75,0.75,0.35,0.55,0.59,0.61,0.56,0.74,0.76,0.04,0.14,0.03,0.24,0.27,0.37,0.39,0.07,0.07,0.08,0.08,0.89,0.06,0.14,0.13,0.33,0.39,0.28,0.55,0.09,0.51,0.5,0.21,0.71,0.52,0.05,0.26,0.65,0.14,0.06,0.22,0.19,0.18,0.36,0.35,0.38,0.34,0.38,0.46,0.25,0.04,0.0,0.12,0.42,0.5,0.51,0.64,0.03,0.13,0.96,0.17,0.06,0.18,0.44,0.13,0.94,0.93,0.03,0.07,0.1,0.07,0.02,0.57,0.29,0.12,0.26,0.2,0.06,0.04,0.9,0.5,0.32,0.14]
 # get result 0.27
@@ -65,10 +78,10 @@ print("Cross-predicted Accuracy:", accuracy)
 
 
 # Save model
-saved_model = pickle.dumps(clf)
+#saved_model = pickle.dumps(clf)
 # Test saved model
 # clf2 = pickle.loads(saved_model)
 # clf2.predict()
 
 # Save model to file
-dump(clf, 'predict_svm.joblib')
+#dump(clf, 'predict_svm.joblib')
